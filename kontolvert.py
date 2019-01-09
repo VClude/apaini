106aa242c987e18db0932aeffb3b433ab67bf80b
Piklude <v@clude.id>
2019-01-09 16:18:17 +0700

    add char remover after convert

--- kontolvert.py
+++ kontolvert.py
@@ -4,16 +4,19 @@ contents = open("isi.txt","r")
 with open("copythis.html", "w") as e:
     for lines in contents.readlines():
         if re.match(r'#', lines):
-            print("<div class='container' id='and" + str(konter) + "'>\n<h3 class='s-title'>"+ lines + "</h3>\n")
-            e.write("<div class='container' id='and" + str(konter) + "'>\n<h3 class='s-title'>"+ lines + "</h3>\n")
+            newlines = lines.replace(r'#', "")
+            print("<div class='container' id='and" + str(konter) + "'>\n<h3 class='s-title'>"+ newlines + "</h3>\n")
+            e.write("<div class='container' id='and" + str(konter) + "'>\n<h3 class='s-title'>"+ newlines + "</h3>\n")
             konter += 1
         elif re.match(r'\s', lines):
-            print("<li>\n" + lines + "</li>\n")
-            e.write("<ul>\n<li>\n" + lines + "</li>\n</ul>\n")
+            newlines = lines.replace(r'\s', "")
+            print("<li>\n" + newlines + "</li>\n")
+            e.write("<ul>\n<li>\n" + newlines + "</li>\n</ul>\n")
             konter += 1
         elif re.match(r'\%', lines):
-            print("<p>\n" + lines + "</p>\n")
-            e.write("<p>\n" + lines + "</p>\n")
+            newlines = lines.replace(r'%', "")
+            print("<p>\n" + newlines + "</p>\n")
+            e.write("<p>\n" + newlines + "</p>\n")
             konter += 1
         elif (r'#|$', lines):
             print("</div>\n")
