74993535d8e351511bb09d06c3355eb18a60d3e7
Piklude <v@clude.id>
2019-01-09 16:04:07 +0700

    neating the format

--- kontolvert.py
+++ kontolvert.py
@@ -4,12 +4,12 @@ contents = open("isi.txt","r")
 with open("copythis.html", "w") as e:
     for lines in contents.readlines():
         if re.match(r'#', lines):
-            print("<div class='container' id='and" + str(konter) + "'><h3 class='s-title'>"+ lines + "</h3>")
-            e.write("<div class='container' id='and" + str(konter) + "'><h3 class='s-title'>"+ lines + "</h3>")
+            print("<div class='container' id='and" + str(konter) + "'>\n<h3 class='s-title'>"+ lines + "</h3>\n")
+            e.write("<div class='container' id='and" + str(konter) + "'>\n<h3 class='s-title'>"+ lines + "</h3>\n")
             konter += 1
         elif re.match(r'\s', lines):
             print("<li>\n" + lines + "</li>\n")
-            e.write("<ul><li>\n" + lines + "</li></ul>\n")
+            e.write("<ul>\n<li>\n" + lines + "</li>\n</ul>\n")
             konter += 1
         elif re.match(r'\%', lines):
             print("<p>\n" + lines + "</p>\n")
